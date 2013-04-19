06ba5f54fb17da9c45aba95b00ab46721eb0dc44
thousandsofthem <thousandsofthem@gmail.com>
2013-04-19 11:49:52 +0300

    small refactor

--- ffmpeg_turn_key/README.md
+++ ffmpeg_turn_key/README.md
@@ -1,19 +1,19 @@
 ### Turn key ffmpeg worker
----
 
 This is full functional demo of video processing worker.
-You need http://iron.io/ account to use it.
+
+Get an [Iron.io](www.iron.io) account and [create your iron.json file](http://dev.iron.io/worker/reference/configuration/). Then run:
 
 ### Instructions
 
-1. Set up amazon s3 credentials. Insert real access_key and secret_key into `ffmpeg_turn_key.config.yml`
+1. Install [cli](http://dev.iron.io/worker/reference/cli/) tool: `gem install iron_worker_ng`
+1. Set up amazon s3 credentials. Insert real access_key, secret_key, bucket into `ffmpeg_turn_key.config.yml`
 1. Upload worker: `iron_worker upload --worker-config ffmpeg_turn_key.config.yml ffmpeg_turn_key`
 1. Use it!
 
 Command-line example - launch worker, wait for finish and print log:
 ```sh
 $ iron_worker queue ffmpeg_turn_key -p '{
-  "bucket":"turn_key_worker_test",
   "store_name":"iron_man.mp4",
   "video_url":"https://s3.amazonaws.com/iron-examples/video/iron_man_2_trailer_official.flv"
 }' --wait
--- ffmpeg_turn_key/ffmpeg_turn_key.rb
+++ ffmpeg_turn_key/ffmpeg_turn_key.rb
@@ -4,7 +4,6 @@ ENV['PATH'] = File.expand_path('./bin', File.dirname(__FILE__)) + ":" + ENV['PAT
 require 'aws/s3'
 
 # Input data
-bucket = params[:bucket] || raise("Please set up bucket")
 store_name = params[:store_name] || raise("Please set up store_name")
 encoding_options = params[:encoding_options] || "-c:v libx264 -preset medium -crf 22 -c:a copy"
 in_file = params[:video_url] || raise("Please set up video_url")
@@ -13,6 +12,7 @@ out_ext = params[:out_format] || 'mp4'
 # Check config status
 config[:amazon][:access_key] || raise("please set up amazon/access_key")
 config[:amazon][:secret_key] || raise("please set up amazon/secret_key")
+config[:amazon][:bucket] || raise("please set up amazon/bucket")
 
 # Download input video file
 `mkdir -p data`
@@ -23,9 +23,7 @@ out_file = "data/output.#{out_ext}"
 puts `ffmpeg -version`
 
 # Convert!
-puts '*'*80
-puts `ffmpeg -i data/input.flv -loglevel error #{encoding_options} #{out_file}`
-puts '*'*80
+puts `ffmpeg -i data/input.flv -loglevel warning #{encoding_options} #{out_file}`
 
 puts "Converted video size: #{File.size(out_file)} Bytes"
 
@@ -35,10 +33,10 @@ AWS::S3::Base.establish_connection!(
   secret_access_key: config[:amazon][:secret_key]
 )
 
-puts AWS::S3::S3Object.store(store_name, open(out_file), bucket)
+puts AWS::S3::S3Object.store(store_name, open(out_file), config[:amazon][:bucket])
 
 puts "Movie url:"
-puts AWS::S3::S3Object.url_for(store_name, bucket, expires:  Time.mktime(2038, 1, 18).to_i)
+puts AWS::S3::S3Object.url_for(store_name, config[:amazon][:bucket], expires:  Time.mktime(2038, 1, 18).to_i)
 
 # Video uploaded, notify parent
 # Implement me
