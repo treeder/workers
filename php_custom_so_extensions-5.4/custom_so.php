a958725787402a1881843d24167d4297bf2c8918
thousandsofthem <thousandsofthem@gmail.com>
2014-04-01 16:33:31 +0300

    added php5.4 example

---
+++ php_custom_so_extensions-5.4/custom_so.php
@@ -0,0 +1,19 @@
+<?php
+
+echo `php --version`;
+
+echo "\nExtensions:\n";
+$exts = get_loaded_extensions();
+
+echo sizeof($exts) . " total\n\n";
+
+foreach($exts as $k => $value){
+	try {
+		$version = phpversion($value);
+		echo "[$k] $value => $version\n";
+	} catch (Exception $e) {
+    echo 'Caught exception: ',  $e->getMessage(), "\n";
+	}
+}
+
+echo "\nEND\n";
